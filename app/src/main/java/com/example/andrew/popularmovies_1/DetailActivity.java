1e2f3ee248b3e37bf077e607598c8f422084658b
aczender <czender.a@gmail.com>
2018-05-01 20:11:34 +0200

    2018.05.01.

--- app/src/main/java/com/example/andrew/popularmovies_1/DetailActivity.java
+++ app/src/main/java/com/example/andrew/popularmovies_1/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.andrew.popularmovies_1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
-import com.example.andrew.popularmovies_1.JsonUtils;
 
 import org.json.JSONArray;
 
@@ -19,64 +19,49 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
-    private PosterAdapter mAdapter;
+    private Poster mPoster;
+    ImageView thumbnailIv;
+    TextView titleTv;
+    TextView plotSynopsisTv;
+    TextView userRatingTv;
+    TextView releaseDateTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView thumbnailIv = findViewById(R.id.thumbnail);
+        //final PosterAdapter postersAdapter = new PosterAdapter(this, new ArrayList<Poster>());
+        //thumbnailIv.setTag(postersAdapter);
+
+        if (getIntent().hasExtra(EXTRA_POSITION)) {
+            mPoster = getIntent().getParcelableExtra(EXTRA_POSITION);
+        } else {
+            throw new IllegalArgumentException("Detail activity must receive a movie parcelable");
+        }
+
+        thumbnailIv = (ImageView) findViewById(R.id.thumbnail);
+        titleTv = (TextView) findViewById(R.id.title_tv);
+        plotSynopsisTv = (TextView) findViewById(R.id.plot_synopsis_tv);
+        userRatingTv = (TextView) findViewById(R.id.user_rating_iv);
+        releaseDateTv = (TextView) findViewById(R.id.release_date_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
-            closeOnError();
-            return;
-        }
 
-        String[] details = getResources().;
-        String jsonResponse = details[position];
-        Poster poster = null;
-        poster = JsonUtils.extractFeatureFromJson();
-
-        if (poster == null) {
-            // Poster data unavailable
-            closeOnError();
-            return;
-        }
-
-        populateUI(poster);
         Picasso.with(this)
-                .load(poster.getImage())
+                .load(mPoster.getImage())
                 .into(thumbnailIv);
-
-        //setTitle(poster.getMainName());
+        titleTv.setText(mPoster.getTitle());
+        plotSynopsisTv.setText(mPoster.getSynopsis());
+        userRatingTv.setText(mPoster.getUserRating());
+        releaseDateTv.setText(mPoster.getReleaseDate());
     }
+
     private void closeOnError() {
         finish();
         Toast.makeText(this, "Movie data not available", Toast.LENGTH_SHORT).show();
-    }
-
-    private void populateUI(Poster poster) {
-
-        TextView titleTv = findViewById(R.id.title_tv);
-        TextView plotSynopsisTv = findViewById(R.id.plot_synopsis_tv);
-        TextView userRatingTv = findViewById(R.id.user_rating_iv);
-        TextView releaseDateTv = findViewById(R.id.release_date_tv);
-
-        Intent intent = getIntent();
-        if (intent == null) {
-            closeOnError();
-        }
-
-        titleTv.setText(poster.getTitle());
-        plotSynopsisTv.setText(poster.getSynopsis());
-        userRatingTv.setText(poster.getUserRating());
-        releaseDateTv.setText(poster.getReleaseDate());
     }
 }
